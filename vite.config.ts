4e47604a76819fb2c4a8628366adacdbfe935151
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-04 18:44:15 +0900

    Init/setting (#2)
    * Init: 프로젝트 세팅
    
    * Chore: eslint + prettier 설정
    
    * Chore: husky 세팅 + lint 설정
    
    * Script: lint-staged 수정
    
    * Chore: storybook 설정 + 폴더 구조 추가
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
