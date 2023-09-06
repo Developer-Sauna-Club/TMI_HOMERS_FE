916ae125d35d32e34886407f652bac44422ac44f
Dongja <jdh981118@naver.com>
2023-09-06 22:56:53 +0900

    feat: react Query Provider 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const MILLISECOND = 1000;
+const SECOND = 60;
+const MINUTE = 5;
+
+const STALE_TIME = MILLISECOND * SECOND * MINUTE;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: STALE_TIME,
+    },
+  },
+});
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">안녕하세요</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl font-bold underline">안녕하세요</h1>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
