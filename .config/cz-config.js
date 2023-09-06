4990c496671e32b2308ebafe931904ca416a81ad
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-09-06 21:24:34 +0900

    chore: eslint 규칙 추가 + cz-config 설정 + alias 설정

---
+++ .config/cz-config.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+
+  types: [
+      { value: 'feat', name: 'feat:\tfeature를 추가하는 경우' },
+      { value: 'fix', name: 'fix:\t코드를 수정하는 경우' },
+      { value: 'docs', name: 'docs:\t문서를 추가하거나 수정하는 경우' },
+      {
+        value: 'style',
+        name: 'style:\t코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우',
+      },
+      { value: 'refactor',
+        name: 'refactor:\t(버그나 기능 추가 X) 코드를 리팩토링하는 경우',
+      },
+      {
+        value: 'test',
+        name: 'test:\t(프로덕션 코드 변경 X) 테스트를 추가하거나 테스트 리팩토링하는 경우',
+      },
+      {
+        value: 'chore',
+        name: 'chore:\t 빌드 태스크 업데이트, 패키지 매니저를 설정하는 경우',
+      },
+  ],
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+  skipQuestions: ['body'],
+  subjectLimit: 100,
+};
