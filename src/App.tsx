7a8f17208f46ad91980e0e1b9298fa1f184279c8
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-06 21:35:16 +0900

    chore: eslint 규칙 추가 + cz-config 설정 + alias 설정 (#6)
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">안녕하세요</h1>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/constants') },
+      { find: '@api', replacement: path.resolve(__dirname, 'src/api') },
+      { find: '@context', replacement: path.resolve(__dirname, 'src/context') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@stories', replacement: path.resolve(__dirname, 'src/stories') },
+      { find: '@types', replacement: path.resolve(__dirname, 'src/types') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
   },
 });
