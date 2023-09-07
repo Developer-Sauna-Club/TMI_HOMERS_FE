c227fc53f60582fe0e2a2939cbfabbe12de11e27
Dongja <jdh981118@naver.com>
2023-09-07 17:58:57 +0900

    feat: input Component UI 디자인

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'input-white': '#FFF',
+        'lazy-gray': '#C0C1C5',
+        'cooled-blue': '#76B7B1',
+        'extra-white': '#EFF0EB',
+        'wall-street': '#697279',
+        'footer-icon': '#455154',
+        'tricorn-black': '#2D2D2D',
+      },
+      fontFamily: {
+        Cafe24Surround: ['Cafe24Surround'],
+        Cafe24SurroundAir: ['Cafe24SurroundAir'],
+        RixInooAriDuriR: ['RixInooAriDuriR'],
+      },
+    },
   },
   plugins: [],
-}
+};
