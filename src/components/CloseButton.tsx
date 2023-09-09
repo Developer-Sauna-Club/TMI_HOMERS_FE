a420cbd714adff3579bf4f2dae4b6230a2232544
colorkite10 <colorkite10@gmail.com>
2023-09-10 00:43:36 +0900

    feat: closebutton 공통 컴포넌트 추가

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,31 @@
+import { AiOutlineClose } from 'react-icons/ai';
+
+type CloseButtonProps = {
+  mode?: 'large' | 'small';
+  type?: 'button';
+  onClick?: () => void;
+};
+
+const BASE_BUTTON_CLASSES = 'block bg-transparent rounded-md text-base';
+
+const ModeClasses = {
+  large: 'w-[1.083rem] h-[1.083rem]',
+  small: 'w-[0.667rem] h-[0.667rem]',
+};
+
+const CloseButton = ({ mode = 'large', type = 'button', onClick, ...props }: CloseButtonProps) => {
+  const computedClasses = ModeClasses[mode];
+
+  return (
+    <button
+      className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
+      type={type}
+      onClick={onClick}
+      {...props}
+    >
+      <AiOutlineClose className="icon" size={`${computedClasses}`} color="black" />
+    </button>
+  );
+};
+
+export default CloseButton;
