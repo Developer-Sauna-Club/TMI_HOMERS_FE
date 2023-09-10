a91e4b6875aa8da8502890b43efc1f3c4f842ba0
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-09-10 16:27:54 +0900

    feat: lineclamp 추가 + IconContext 제거

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,3 @@
-import { IconContext } from 'react-icons';
 import { BiSolidComment, BiImageAlt } from 'react-icons/bi';
 import { BsFire } from 'react-icons/bs';
 import { HiThumbUp } from 'react-icons/hi';
@@ -59,19 +58,17 @@ const Article = ({ title, nickname, postedDate, hasImage, likes, comments }: Art
   const isHighlyLiked = likes >= 15;
 
   return (
-    <div className="w-[22.375rem] pl-4 pr-3 pb-[0.625rem] pt-[0.25rem] font-Cafe24SurroundAir">
+    <div className="max-w-[22.375rem] pl-4 pr-3 pb-[0.625rem] pt-[0.25rem] font-Cafe24SurroundAir">
       <div className="flex items-center justify-between mb-2">
         <div className="flex items-center">
           {isHighlyLiked && (
-            <IconContext.Provider value={{ className: 'text-article-highly-liked mr-[0.25rem]' }}>
-              <BsFire className="w-[1rem] h-[1rem]" />
-            </IconContext.Provider>
+            <BsFire className="text-article-highly-liked mr-[0.25rem] w-[1rem] h-[1rem]" />
           )}
-          <h1 className="text-tricorn-black dark:text-extra-white">{title}</h1>
+          <h1 className="text-tricorn-black dark:text-extra-white line-clamp-1 max-w-[16.625rem]">
+            {title}
+          </h1>
           {hasImage && (
-            <IconContext.Provider value={{ className: 'text-article-img dark:text-extra-white' }}>
-              <BiImageAlt className="w-[1rem] h-[1rem] ml-[0.25rem]" />
-            </IconContext.Provider>
+            <BiImageAlt className="text-article-img dark:text-extra-white w-[1rem] h-[1rem] ml-[0.25rem]" />
           )}
         </div>
         <span className="text-lazy-gray text-[0.3rem]">{timestamp}</span>
@@ -79,21 +76,15 @@ const Article = ({ title, nickname, postedDate, hasImage, likes, comments }: Art
       <div className="flex justify-between h-[0.75rem]">
         <div className="flex items-center h-full">
           <div className="flex justify-center mr-[1.7rem]">
-            <IconContext.Provider
-              value={{
-                className: `mr-[0.25rem] ${
-                  isHighlyLiked ? 'text-article-highly-liked' : 'text-lazy-gray'
-                }`,
-              }}
-            >
-              <HiThumbUp className="w-[0.9rem]" />
-            </IconContext.Provider>
+            <HiThumbUp
+              className={`w-[0.9rem] mr-[0.25rem] ${
+                isHighlyLiked ? 'text-article-highly-liked' : 'text-lazy-gray'
+              }`}
+            />
             <span className="text-wall-street text-[0.75rem] h-[0.75rem]">{likes}</span>
           </div>
           <div className="flex justify-center">
-            <IconContext.Provider value={{ className: 'text-lazy-gray mr-[0.25rem]' }}>
-              <BiSolidComment className="w-[0.9rem]" />
-            </IconContext.Provider>
+            <BiSolidComment className="w-[0.9rem] text-lazy-gray mr-[0.25rem]" />
             <span className="text-wall-street text-[0.75rem] h-[0.75rem]">{comments}</span>
           </div>
         </div>
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,46 +1,50 @@
-import { IconContext } from 'react-icons';
 import { BiSolidPencil } from 'react-icons/bi';
 import { FaUser } from 'react-icons/fa6';
 
 type AvatarProps = {
+  width: number;
   profileImage: string;
   isLoggedIn: boolean;
 };
 
-const PROFILE_STYLE =
-  'w-[8rem] h-[8rem] relative rounded-full border-[0.1rem] border-lazy-gray bg-profile-bg';
+const Avatar = ({ width, profileImage, isLoggedIn }: AvatarProps) => {
+  const PROFILE_STYLE = `relative rounded-full border-[0.1rem] border-lazy-gray bg-profile-bg`;
+  const userIconWidth = width * 0.55;
+  const editIconWidth = width * 0.1875;
+  const editPencilWidth = width * 0.125;
+  const editPencilPosition = width * 0.0625;
 
-const Avatar = ({ profileImage, isLoggedIn }: AvatarProps) => {
   return (
-    <div className="relative">
+    <div className="relative inline-block ">
       {profileImage !== '' ? (
         <img
           src={`data:image/jpeg;base64,${profileImage}`}
           alt="프로필 이미지"
+          style={{ width: `${width}rem`, height: `${width}rem` }}
           className={PROFILE_STYLE}
         />
       ) : (
-        <div className={PROFILE_STYLE}>
-          <IconContext.Provider
-            value={{
-              className:
-                'w-[4.76rem] h-[4.76rem] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-footer-icon',
-            }}
-          >
-            <FaUser />
-          </IconContext.Provider>
+        <div className={PROFILE_STYLE} style={{ width: `${width}rem`, height: `${width}rem` }}>
+          <FaUser
+            style={{ width: `${userIconWidth}rem`, height: `${userIconWidth}rem` }}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-footer-icon"
+          />
         </div>
       )}
       {isLoggedIn && (
-        <div className="relative w-[1.5rem] h-[1.5rem] rounded-full bg-profile-bg border-lazy-gray border-[0.1rem] left-[6rem] -top-[2rem]">
-          <IconContext.Provider
-            value={{
-              className:
-                'w-[1rem] h-[1rem] absolute absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ',
-            }}
-          >
-            <BiSolidPencil />
-          </IconContext.Provider>
+        <div
+          style={{
+            width: `${editIconWidth}rem`,
+            height: `${editIconWidth}rem`,
+            right: `${editPencilPosition}rem`,
+            bottom: `${editPencilPosition}rem`,
+          }}
+          className="absolute rounded-full bg-profile-bg border-lazy-gray border-[0.1rem]"
+        >
+          <BiSolidPencil
+            style={{ width: `${editPencilWidth}rem`, height: `${editPencilWidth}rem` }}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "
+          />
         </div>
       )}
     </div>
