a9b76cae8a765d44b8085f5472b83329b5c08647
Dongho Shin <kakkiid@gmail.com>
2023-09-10 16:22:22 +0900

    feat: MainButton 공통 컴포넌트 추가 (#19)
    * feat: mainbutton 공통 컴포넌트 추가
    
    * fix: outlined 모드에서 텍스트 색 화이트로 나오는 부분 수정
    
    * fix: px to rem

---
+++ src/components/MainButton.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from 'react';
+
+type MainButtonProps = {
+  label: string;
+  mode?: 'filled' | 'outlined';
+  type?: 'button' | 'submit' | 'reset';
+  onClick?: () => void;
+};
+
+const BASE_BUTTON_CLASSES = 'block rounded-md text-base w-[18.375rem] h-[3.125rem]';
+
+const getModeClasses = (mode: MainButtonProps['mode']) => {
+  if (mode === 'filled') {
+    return 'bg-footer-icon text-white font-Cafe24Surround hover:bg-wall-street';
+  } else if (mode === 'outlined') {
+    return 'bg-transparent border border-1 border-wall-street text-wall-street font-Cafe24SurroundAir hover:bg-footer-icon hover:border-none hover:text-white';
+  }
+};
+
+const MainButton = ({
+  label = '메인버튼',
+  mode = 'filled',
+  type = 'button',
+  onClick,
+  ...props
+}: MainButtonProps) => {
+  const computedClasses = useMemo(() => {
+    return getModeClasses(mode);
+  }, [mode]);
+
+  return (
+    <button
+      className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
+      type={type}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default MainButton;
