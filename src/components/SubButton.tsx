676020d4c09ad54b9944f20947eb70e752f6c674
daeun <gbajs754@gmail.com>
2023-09-11 20:06:51 +0900

    fix: pR 수정사항 반영

--- src/components/SubButton.tsx
+++ src/components/SubButton.tsx
@@ -1,23 +1,23 @@
 type outlineBtnProps = {
-  color: 'blue' | 'red' | 'violet';
-  weight: 'bold' | 'air';
-  radius: 'small' | 'medium';
+  color?: 'blue' | 'red' | 'violet';
+  weight?: 'bold' | 'air';
+  radius?: 'small' | 'medium';
   type: 'fill' | 'outline';
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
   children: React.ReactNode;
 };
 
 const outLineType = {
-  blue: 'border-cooled-blue text-cooled-blue',
-  red: 'border-warning-red text-warning-red',
-  violet: 'border-light-violet text-light-violet',
+  blue: 'border-cooled-blue text-cooled-blue hover:bg-cooled-blue hover:text-white',
+  red: 'border-error-red text-error-red hover:bg-error-red hover:text-white',
+  violet: 'border-light-violet text-light-violet hover:bg-light-violet hover:text-white',
 };
 
 const fillType = {
-  blue: 'bg-cooled-blue text-white hover:bg-opacity-75',
-  red: 'bg-warning-red text-white  hover:bg-opacity-75',
-  violet: 'bg-light-violet text-white hover:bg-opacity-75',
+  blue: 'bg-cooled-blue text-white hover:bg-opacity-75 border-0',
+  red: 'bg-error-red text-white  hover:bg-opacity-75 border-0',
+  violet: 'bg-light-violet text-white hover:bg-opacity-75 border-0',
 };
 const fontWeight = {
   bold: 'font-Cafe24Surround',
@@ -30,9 +30,9 @@ const borderRadius = {
 
 const SubOutlineButton = ({
   children,
-  color,
-  weight,
-  radius,
+  color = 'blue',
+  weight = 'air',
+  radius = 'small',
   type,
   width,
   height,
@@ -41,7 +41,7 @@ const SubOutlineButton = ({
   return (
     <button
       style={{ width: `${width}rem`, height: `${height}rem` }}
-      className={`border-[2px] px-4 pt-2 pb-1 cursor-pointer transform transition duration-100 focus:scale-95 ${
+      className={`border-2 px-4 pt-2 pb-1 cursor-pointer transform transition duration-100 focus:scale-95 ${
         fontWeight[weight]
       } ${borderRadius[radius]} ${type === 'outline' ? outLineType[color] : fillType[color]}`}
       {...props}
