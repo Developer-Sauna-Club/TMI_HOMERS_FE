9a1d6dda08348a7331a33db447cfd15042de39e7
Dongja <jdh981118@naver.com>
2023-09-11 16:29:32 +0900

    design: 알림 컴포넌트

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ export default {
         'wall-street': '#697279',
         'footer-icon': '#455154',
         'tricorn-black': '#2D2D2D',
+        'tertiory-gray': '#9BA5B7',
+        'primary-black': '#141619',
       },
       fontFamily: {
         Cafe24Surround: ['Cafe24Surround'],
