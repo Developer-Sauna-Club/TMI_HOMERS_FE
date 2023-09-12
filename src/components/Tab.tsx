c02455482a1cad04392048d542aad2cd83f1fd74
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-12 14:16:14 +0900

    design: Subscribe, Tab, TabItem 공통 컴포넌트 추가 (#45)
    * feat: subscribe 공통 컴포넌트 추가 (+storybook)
    
    * feat: tab, TabItem 공통 컴포넌트 추가 (+storybook)
    
    * feat: tabContents enum 파일 추가 + 유틸 함수 추가
    
    * feat: subscribeInfo로 이름 변경 + K 포맷팅 유틸 함수 추가
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

---
+++ src/components/Tab.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo, useState } from 'react';
+import TabItem from './TabItem';
+
+const childrenToArray = (children: React.ReactNode, ...types: string[]): React.ReactElement[] => {
+  return React.Children.toArray(children).reduce<React.ReactElement[]>((acc, element) => {
+    if (React.isValidElement(element) && types.includes(element.props.__TYPE)) {
+      acc.push(element);
+    }
+    return acc;
+  }, []);
+};
+
+type TabProps = {
+  children: React.ReactNode;
+  active?: boolean;
+};
+
+const Tab = ({ children, active }: TabProps) => {
+  const [currentActive, setCurrentActive] = useState(() => {
+    if (active) {
+      return active;
+    } else {
+      const index = childrenToArray(children, 'Tab.Item')[0].props.index;
+      return index;
+    }
+  });
+
+  const items = useMemo(() => {
+    return childrenToArray(children, 'Tab.Item').map((element) => {
+      return React.cloneElement(element, {
+        ...element.props,
+        key: element.props.index,
+        active: element.props.index === currentActive,
+        onClick: () => {
+          setCurrentActive(element.props.index);
+        },
+      });
+    });
+  }, [children, currentActive]);
+
+  const activeItem = useMemo(
+    () => items.find((element) => currentActive === element.props.index),
+    [currentActive, items],
+  );
+
+  return (
+    <div>
+      <div className="flex items-center">{items}</div>
+      <div>{activeItem?.props.children}</div>
+    </div>
+  );
+};
+
+Tab.Item = TabItem;
+
+export default Tab;
