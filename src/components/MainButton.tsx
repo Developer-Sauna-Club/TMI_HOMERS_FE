aad14a63b1e6870d7035818eb5049197175414c5
Dongja <jdh981118@naver.com>
2023-09-12 18:11:25 +0900

    design: 회원가입 페이지 기초 레이아웃

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,33 +1,42 @@
+import Loader from '@components/Loader';
+
 type MainButtonProps = {
   label: string;
   mode?: 'filled' | 'outlined';
   type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
+  isLoading?: boolean;
 };
 
 const BASE_BUTTON_CLASSES = 'rounded-md text-base w-[18.375rem] h-[3.125rem]';
 const modeClasses = {
   filled: 'bg-footer-icon text-white font-Cafe24Surround hover:bg-wall-street',
-  outlined: 'bg-transparent border border-1 border-wall-street text-wall-street font-Cafe24SurroundAir hover:bg-footer-icon hover:border-none hover:text-white'
-}
+  outlined:
+    'bg-transparent border border-1 border-wall-street text-wall-street font-Cafe24SurroundAir hover:bg-footer-icon hover:border-none hover:text-white',
+};
 
 const MainButton = ({
   label = '메인버튼',
   mode = 'filled',
   type = 'button',
+  isLoading = false,
   onClick,
   ...props
 }: MainButtonProps) => {
   const computedClasses = modeClasses[mode];
 
+  const handleClick = () => {
+    onClick && onClick();
+  };
+
   return (
     <button
       className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
       type={type}
-      onClick={onClick}
+      onClick={handleClick}
       {...props}
     >
-      {label}
+      {isLoading ? <Loader /> : label}
     </button>
   );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,6 +1,8 @@
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import MainButton from '@/components/MainButton';
 import FormInput from '@components/FormInput';
-import Loader from '@components/Loader';
+import Header from '@components/HeaderText';
 import useSignUp from '@hooks/useSignUp';
 
 type SignUpFormValues = {
@@ -16,80 +18,92 @@ const SignUpPage = () => {
   const onSubmit: SubmitHandler<SignUpFormValues> = ({ email, password, nickname }) => {
     signUpMutate({ email, password, nickname });
   };
+  const navigate = useNavigate();
+
+  const handleClickLoginLink = () => {
+    navigate('/login');
+  };
 
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="">회원가입 페이지</h2>
+    <div className="flex flex-col items-center h-[100vh]">
+      <Header size="large" label="회원 가입" />
       <FormProvider {...methods}>
         <form
           onSubmit={methods.handleSubmit(onSubmit)}
-          className="flex flex-col font-bold p-4 px-16"
+          className="flex flex-col items-center font-bold gap-4"
         >
-          <FormInput
-            name="email"
-            label="이메일"
-            registerOptions={{
-              required: '이메일은 필수입니다!',
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: '이메일 형식에 맞지 않습니다!',
-              },
-            }}
-            type="email"
-            placeholder="이메일을 입력해주세요"
-          />
-          <FormInput
-            name="password"
-            label="비밀번호"
-            registerOptions={{
-              required: '비밀번호는 필수입니다!',
-              pattern: {
-                value: /^[A-Za-z0-9@$!%*#?&]+$/,
-                message: '영문, 숫자, 특수기호로 입력해주세요!',
-              },
-              minLength: {
-                value: 8,
-                message: '8글자 이상 입력해주세요!',
-              },
-            }}
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-          />
-          <FormInput
-            name="passwordCheck"
-            label="비밀번호확인"
-            registerOptions={{
-              required: '비밀번호 확인은 필수입니다!',
-              validate: (value, { password }) =>
-                value === password || '비밀번호와 일치하지 않습니다!',
-            }}
-            type="password"
-            placeholder="비밀번호를 한번 더 입력해주세요!"
-          />
-          <FormInput
-            name="nickname"
-            label="닉네임"
-            registerOptions={{
-              required: '닉네임은 필수입니다!',
-              pattern: {
-                value: /^[A-Za-z0-9가-힣]+$/,
-                message: '한글, 영문, 숫자로 입력해주세요!',
-              },
-              minLength: {
-                value: 2,
-                message: '2글자 이상으로 입력해주세요!',
-              },
-              maxLength: {
-                value: 10,
-                message: '10글자 이하로 입력해주세요!',
-              },
-            }}
-            placeholder="닉네임을 입력해주세요"
-          />
-          <button className="mt-2 p-2" disabled={isLoading}>
-            {isLoading && <Loader />}
-            {!isLoading && '회원가입'}
-          </button>
+          <div className="w-full">
+            <FormInput
+              name="email"
+              label="이메일"
+              registerOptions={{
+                required: '이메일은 필수입니다!',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: '이메일 형식에 맞지 않습니다!',
+                },
+              }}
+              type="email"
+              placeholder="이메일을 입력해주세요"
+            />
+            <FormInput
+              name="password"
+              label="비밀번호"
+              registerOptions={{
+                required: '비밀번호는 필수입니다!',
+                pattern: {
+                  value: /^[A-Za-z0-9@$!%*#?&]+$/,
+                  message: '영문, 숫자, 특수기호로 입력해주세요!',
+                },
+                minLength: {
+                  value: 8,
+                  message: '8글자 이상 입력해주세요!',
+                },
+              }}
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <FormInput
+              name="passwordCheck"
+              label="비밀번호 확인"
+              registerOptions={{
+                required: '비밀번호 확인은 필수입니다!',
+                validate: (value, { password }) =>
+                  value === password || '비밀번호와 일치하지 않습니다!',
+              }}
+              type="password"
+              placeholder="비밀번호를 한번 더 입력해주세요!"
+            />
+            <FormInput
+              name="nickname"
+              label="닉네임"
+              registerOptions={{
+                required: '닉네임은 필수입니다!',
+                pattern: {
+                  value: /^[A-Za-z0-9가-힣]+$/,
+                  message: '한글, 영문, 숫자로 입력해주세요!',
+                },
+                minLength: {
+                  value: 2,
+                  message: '2글자 이상으로 입력해주세요!',
+                },
+                maxLength: {
+                  value: 10,
+                  message: '10글자 이하로 입력해주세요!',
+                },
+              }}
+              placeholder="닉네임을 입력해주세요"
+            />
+          </div>
+          <div className="flex flex-col">
+            <MainButton label="회원가입" type="submit" isLoading={isLoading} />
+            <p
+              className="p-2 self-end hover:underline hover:opacity-75"
+              onClick={handleClickLoginLink}
+            >
+              로그인 페이지로 이동
+            </p>
+          </div>
         </form>
       </FormProvider>
     </div>
