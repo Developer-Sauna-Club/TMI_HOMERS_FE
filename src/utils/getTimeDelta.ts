9a2d77ad1ebd356d8472f75761cbcfb3ee8789a5
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-12 16:57:37 +0900

    refactor: Tab, Article 공통 컴포넌트 수정 (#63)
    * refactor: article 아이콘 정렬 + line-clamp 오류 수정 + 유틸 함수 분리
    
    * refactor: 제거된 Input 공통 컴포넌트 제거 + max-width를 가지도록 수정
    
    * refactor: tab 컴포넌트 width -\> maxWidth로 수정
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

---
+++ src/utils/getTimeDelta.ts
@@ -0,0 +1,40 @@
+export const getTimeDelta = (postedDate: string) => {
+  const dt = new Date(postedDate);
+  const now = new Date();
+  const diff = now.getTime() - dt.getTime();
+
+  if (isNaN(diff)) {
+    return '알 수 없음';
+  }
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  let result = '';
+
+  switch (true) {
+    case seconds < 60:
+      result = `${seconds}초 전`;
+      break;
+    case minutes < 60:
+      result = `${minutes}분 전`;
+      break;
+    case hours < 24:
+      result = `${hours}시간 전`;
+      break;
+    case days < 30:
+      result = `${days}일 전`;
+      break;
+    case months < 12:
+      result = `${months}달 전`;
+      break;
+    default:
+      result = `${years}년 전`;
+  }
+
+  return result;
+};
