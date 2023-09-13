3b420121e6c461e6039514d1c2125f1bd5a828d4
Dongho Shin <kakkiid@gmail.com>
2023-09-13 18:16:20 +0900

    feat: 작성 글 API 요청 기능 추가

--- src/pages/NewArticlePage.tsx
+++ src/pages/NewArticlePage.tsx
@@ -1,47 +1,82 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 import { checkAuthentication } from '@/api/auth';
-// import { axiosClient } from '@/api/axiosClient';
+import { axiosClient } from '@/api/axiosClient';
 import Avatar from '@/components/Avatar';
 import CloseButton from '@/components/CloseButton';
 import SubButton from '@/components/SubButton';
 import { User } from '@/type/User';
+import { getItemFromStorage } from '@/utils/localStorage';
 
-// TODO api 요청부 수정
-// const NEW_ARTICLE_URL = '/posts/create';
-// const createArticle = async () => {
-//   const response = await axiosClient.post(NEW_ARTICLE_URL);
-//   return response;
-// };
-
-const getUserData = async () => {
-  const userData = await checkAuthentication();
-  return userData;
+type FormData = {
+  postTitle: string;
+  postBody: string;
 };
 
-// TODO input validation => React-hook-form 사용하기
-// TODO auth 확인 후 api 보내기? auth 되었다고 가정하기? 양자택일
+const CHANNEL_ID = '64fac2e729260903240d2dab';
+const NEW_ARTICLE_URL = '/posts/create';
 
 const NewArticlePage = () => {
+  const { handleSubmit, register } = useForm<FormData>();
+  const navigate = useNavigate();
+
   const [userData, setUserData] = useState<User | undefined>(undefined);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  // NOTE 아이쒸 auth된 유저만 화면이 보이고, 아니면 LoginPage로 라우터 이동시켜주고 싶음.
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    await createArticle(data);
+  };
+
+  const getUserData = async () => {
+    const userData = await checkAuthentication();
+    if (userData) {
+      return userData;
+    }
+  };
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const userDataResponse = await getUserData();
-        setUserData(userDataResponse);
-        setIsLoggedIn(true);
+        if (userDataResponse) {
+          setUserData(userDataResponse);
+          setIsLoggedIn(true);
+        } else {
+          // 토큰이 없으면 로그인 페이지로 리다이렉션
+          setTimeout(() => {
+            navigate('/login');
+          }, 2500);
+        }
       } catch (error) {
-        alert('유저 데이터 페칭 실패');
+        alert('User data fetch failed.');
       }
     };
 
     if (!isLoggedIn) {
       fetchUserData(); // 사용자 데이터 가져오기
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, navigate]);
+
+  const TOKEN = JSON.parse(getItemFromStorage('token')!);
+
+  // TODO api 요청부 수정
+  const createArticle = async (form: FormData) => {
+    const formData = new FormData();
+    formData.append(
+      'title',
+      JSON.stringify({ postTitle: form.postTitle, postBody: form.postBody }),
+    );
+    formData.append('channelId', CHANNEL_ID);
+
+    const response = await axiosClient.post(NEW_ARTICLE_URL, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    return response;
+  };
 
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-white">
@@ -51,7 +86,7 @@ const NewArticlePage = () => {
             <h1 className="font-bold font-Cafe24Surround text-tricorn-black text-[2rem] text-center">
               글 쓰기
             </h1>
-            <Link to={`/home`}>
+            <Link to={`/news`}>
               <CloseButton />
             </Link>
           </div>
@@ -62,27 +97,33 @@ const NewArticlePage = () => {
                 {userData ? userData.fullName : '닉네임'}
               </p>
             </div>
-            <SubButton type="fill">작성하기</SubButton>
           </div>
           <div className="w-full">
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="제목을 입력해주세요"
-              className="w-full bg-white border border-lazy-gray rounded-lg p-2 text-tricorn-black"
-            />
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="내용을 입력해주세요"
-              className="w-full bg-white border border-lazy-gray rounded-lg mt-5 h-[300px] p-2 text-tricorn-black"
-            />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                {...register('postTitle', {
+                  required: 'Required',
+                })}
+                placeholder="제목을 입력해주세요"
+                className="w-full bg-white border border-lazy-gray rounded-lg p-2 text-tricorn-black"
+              />
+              <input
+                type="text"
+                {...register('postBody', {
+                  required: 'Required',
+                })}
+                placeholder="내용을 입력해주세요"
+                className="w-full bg-white border border-lazy-gray rounded-lg mt-5 h-[300px] p-2 text-tricorn-black"
+              />
+              <div className="text-right mt-5">
+                <SubButton type="fill">작성하기</SubButton>
+              </div>
+            </form>
           </div>
         </div>
       ) : (
-        <p>로그인이 필요합니다.</p>
+        <p>로그인이 필요합니다! 로그인 페이지로 이동합니다...</p>
       )}
     </div>
   );
