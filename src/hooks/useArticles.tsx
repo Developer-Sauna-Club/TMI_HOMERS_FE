1e2f76aa9c22c30c8e195636a3da81cccd7fe3fb
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-09-13 18:00:55 +0900

    feat: useArticles 커스텀 훅에 staleTime 추가

--- src/hooks/useArticles.tsx
+++ src/hooks/useArticles.tsx
@@ -5,12 +5,18 @@ import { axiosClient } from '@api/axiosClient';
 import { API } from '@constants/Article';
 
 export const useArticles = () => {
-  const { data, isFetching } = useQuery<AxiosResponse<Post[]>>(['articles'], async () => {
-    const response = await axiosClient.get(
-      `${API.ARTICLES_URL}${API.CHANNEL_URL}/${API.CHANNEL_ID}`,
-    );
-    return response;
-  });
+  const { data, isFetching } = useQuery<AxiosResponse<Post[]>>(
+    ['articles'],
+    async () => {
+      const response = await axiosClient.get(
+        `${API.ARTICLES_URL}${API.CHANNEL_URL}/${API.CHANNEL_ID}`,
+      );
+      return response;
+    },
+    {
+      staleTime: 1000 * 5,
+    },
+  );
 
   return { data, isFetching };
 };
