6b38dbb828ed543c34a0e0833cae9067a52a32e5
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-09-13 17:16:58 +0900

    feat: articlesPage 관련 상수 추가

--- src/constants/Article.ts
+++ src/constants/Article.ts
@@ -1,4 +1,13 @@
-export const CHANNEL_ID = '64fac2e729260903240d2dab';
+export const API = {
+  ARTICLES_URL: '/posts',
+  CHANNEL_URL: '/channel',
+  CHANNEL_ID: '64fac2e729260903240d2dab',
+};
+
+export const ROUTES = {
+  ARTICLES_URL: '/news',
+};
+
 export const HOTTEST_ARTICLE_LIKES_THRESHOLD = 15;
 export const ARTICLE_TITLE_MAX_LENGTH = 20;
 export const ARTICLE_BODY_MAX_LENGTH = 500;
