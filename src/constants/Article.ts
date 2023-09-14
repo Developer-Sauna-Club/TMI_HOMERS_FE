affeb416b694d82a689543e491bdd6b7831001ae
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-14 18:04:15 +0900

    feat: 게시판 페이지 레이아웃 구현 (초안) (#73)
    * refactor: article 공통 컴포넌트 상수 처리
    
    * feat: articlesPage 페이지 추가
    
    * feat: articles 가져오기 + 필터링 커스텀 훅 추가
    
    * feat: article 공통 컴포넌트 props에 id 추가
    
    * feat: articlesPage 관련 상수 추가
    
    * feat: articlesPage 페이지 커스텀 훅 분리
    
    * fix: article에서 사용한 useNavigate관련 오류 처리
    
    * fix: articlesPage 예외 처리 추가
    
    * feat: useArticles 커스텀 훅에 staleTime 추가
    
    * fix: 마지막 커밋 오류
    
    * feat: tab 이동을 위한 onClick 함수 추가
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

---
+++ src/constants/Article.ts
@@ -0,0 +1,13 @@
+export const API = {
+  ARTICLES_URL: '/posts',
+  CHANNEL_URL: '/channel',
+  CHANNEL_ID: '64fac2e729260903240d2dab',
+};
+
+export const ROUTES = {
+  ARTICLES_URL: '/news',
+};
+
+export const HOTTEST_ARTICLE_LIKES_THRESHOLD = 15;
+export const ARTICLE_TITLE_MAX_LENGTH = 20;
+export const ARTICLE_BODY_MAX_LENGTH = 500;
