098027f79d0f05536705c5493367b18ee48070f1
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-09-15 18:32:58 +0900

    refactor: Tab, TabItem, ArticlesPage 리팩토링 (#90)
    * refactor: contextAPI로 Tab, TabItem 관리하도록 수정
    
    * refactor: contextAPI를 사용하여 Tab외부에서도 TabItem 사용할 수 있도록 수정
    
    * refactor: storybook 파일 수정 (Tab, TabItem)
    
    * refactor: articlesPage 수정 (Tab, TabItem)
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

--- src/components/TabItem.tsx
+++ src/components/TabItem.tsx
@@ -1,44 +1,14 @@
-import PropTypes from 'prop-types';
+import { useTabContext } from '@hooks/useTabContext';
 
-type TabItemProps = {
+interface TabItemProps {
   title: string;
-  active?: boolean;
   index: string;
-  icon?: React.ReactNode;
-  width: string;
   children: React.ReactNode;
-  onClick?: () => void;
-};
+}
 
-const TabItem = ({ title, active, icon, width, onClick }: TabItemProps) => {
-  const activeText = active ? 'text-cooled-blue' : 'text-lazy-gray';
-  return (
-    <div
-      style={{ width: `${width}rem` }}
-      className={`inline-block cursor-pointer mb-2 font-Cafe24Surround ${
-        active ? 'border-b-2 border-cooled-blue text-cooled-blue' : 'border-b-2'
-      }`}
-      onClick={onClick}
-    >
-      <div className="flex items-center justify-center mb-1 h-[1.5rem] text-lazy-gray">
-        {icon && <span className={`mb-1 ${activeText}`}>{icon}</span>}
-        <span
-          style={{ fontWeight: active ? 'bold' : 'normal' }}
-          className={`text-[1.125rem] ${activeText}`}
-        >
-          {title}
-        </span>
-      </div>
-    </div>
-  );
-};
-
-TabItem.defaultProps = {
-  __TYPE: 'Tab.Item',
-};
-
-TabItem.propTypes = {
-  __TYPE: PropTypes.oneOf(['Tab.Item']),
+const TabItem = ({ index, children }: TabItemProps) => {
+  const { activeTab } = useTabContext();
+  return <div className="overflow-y-auto max-h-[41rem]">{activeTab === index && children}</div>;
 };
 
 export default TabItem;
