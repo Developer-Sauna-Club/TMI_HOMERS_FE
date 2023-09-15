c6485fa45a70f8c986f0ea8b169c680c894602c6
khakhiD <bfwanso@naver.com>
2023-09-15 18:52:11 +0900

    feat: 기초 상수 및 태그 구조

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import MainButton from '@/components/MainButton';
+
+const LOGO_SRC = '/img/logo.svg';
+const CHARACTER_SRC = '';
+const START_BUTTON_CONTENT = '시작하기';
+
 const LandingPage = () => {
-  return <h2>랜딩 페이지입니다</h2>;
+  const navigate = useNavigate();
+  const handleClickStartButton = () => {
+    navigate('/home');
+  };
+
+  return (
+    <div>
+      <div>
+        <img src={LOGO_SRC} alt="logo" />
+        <img src={CHARACTER_SRC} alt="character" />
+      </div>
+      <div>
+        <p>여러분만의 TMI 뉴스를 마음껏 공유해주세요</p>
+      </div>
+      <div>
+        <MainButton label={START_BUTTON_CONTENT} onClick={handleClickStartButton} />
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
