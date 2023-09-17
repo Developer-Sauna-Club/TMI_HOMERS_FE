29111b3515285f32937750b5da144554f0065a3b
Dongho Shin <kakkiid@gmail.com>
2023-09-17 17:22:29 +0900

    refactor: 캐치프레이즈 수정

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,6 +3,8 @@ import MainButton from '@/components/MainButton';
 
 const LOGO_SRC = '/img/logo.svg';
 const CHARACTER_SRC = '/img/character.png';
+
+const CATCHPHRASE = '쓸데없는 걸 쓰고 싶은데<br/>어디 쓸 데 없나?';
 const START_BUTTON_CONTENT = '시작하기';
 
 const LandingPage = () => {
@@ -17,9 +19,7 @@ const LandingPage = () => {
         <img src={LOGO_SRC} className="w-[48%] max-w-[300px]" alt="logo" />
         <img src={CHARACTER_SRC} className="w-[44%] max-w-[500px]" alt="character" />
         <p className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
-          쓸데없는 것 쓰고 싶을 때
-          <br />
-          쓰고 싶은 곳 호머스
+          {CATCHPHRASE}
         </p>
       </div>
       <div className="mt-[5rem]">
