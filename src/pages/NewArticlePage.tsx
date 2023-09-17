bd2924ed05e5b44c7cbb0c7fb33a3d8a9e0e0107
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-09-17 16:57:50 +0900

    design: 글 작성 페이지 레이아웃 (#105)
    * design: 글쓰기 페이지 레이아웃 구현
    
    * refactor: 스타일 속성 수정
    
    * feat: 이미지 업로드 (1)
    
    * feat: 드롭다운으로 텍스트 템플릿 선택 가능
    
    * design: 드롭다운 텍스트 크기 줄임
    
    * feat: 로그인하기 (#92)
    
    * feat: 로그인 api 및 임시 레이아웃
    
    * rename: login 함수 이름 수정
    
    * feat: 로그인 훅 추가
    
    * feat: 로그인하기
    
    * feat: 로그인 실패 피드백 추가
    
    * feat: 404 pag (#95)
    
    * refactor: Tab, TabItem, ArticlesPage 리팩토링 (#90)
    
    * refactor: contextAPI로 Tab, TabItem 관리하도록 수정
    
    * refactor: contextAPI를 사용하여 Tab외부에서도 TabItem 사용할 수 있도록 수정
    
    * refactor: storybook 파일 수정 (Tab, TabItem)
    
    * refactor: articlesPage 수정 (Tab, TabItem)
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>
    
    * refactor: subButton 컴포넌트 코드 수정 (#99)
    
    * feat: 이미지 업로드(2)
    
    * refactor: 상수 변수로 매직넘버 관리
    
    * refactor: 훅과 로직 분리
    
    * refactor: image 타입 (1)
    
    * feat: 글 작성하기 페이지 레이아웃 구현
    
    * rename: 파일 경로 변경
    
    * fix: image type 수정
    
    * docs: todo 설정
    
    ---------
    
    Co-authored-by: Dongho Shin <kakkiid@gmail.com>
    Co-authored-by: Changuk Woo <43228743+wukdddang@users.noreply.github.com>
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>
    Co-authored-by: Kimdaeun <104294861+GBAJS754@users.noreply.github.com>

--- src/pages/NewArticlePage.tsx
+++ src/pages/NewArticlePage.tsx
@@ -1,5 +1,186 @@
+import { ChangeEvent, FormEventHandler, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { AiOutlinePlus } from 'react-icons/ai';
+import Avatar from '@/components/Avatar';
+import CloseButton from '@/components/CloseButton';
+import HeaderText from '@/components/HeaderText';
+import { DROPDOWN_OPTIONS, LENGTH_LIMIT, MESSAGE } from '@/constants/NewArticle';
+import { useArticle } from '@/hooks/useArticle';
+
+// TODO: 사용자 로그인 정보 불러오기
+// TODO: 작성한 게시물 id값 불러오기
+
+type FormValueType = {
+  title: string;
+  body: string;
+  image: File;
+};
+
 const NewArticlePage = () => {
-  return <h2>글 작성 페이지입니다!</h2>;
+  const navigate = useNavigate();
+  const [selectedText, setSelectedText] = useState('');
+  const [titleCount, setTitleCount] = useState(0);
+
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<FormValueType>();
+  const addArticle = useArticle();
+  const [image, setImage] = useState<File | null>(null);
+
+  const onSubmit: SubmitHandler<FormValueType> = (data) => {
+    try {
+      alert(JSON.stringify(data));
+      addArticle.mutate({ ...data, image });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const handleChangeImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const imageFile = event.target.files;
+    if (!imageFile || imageFile.length < 0) {
+      return;
+    }
+    setImage(imageFile[0]);
+  };
+
+  const handleResizeTextareaHeight: FormEventHandler<HTMLTextAreaElement> = (event) => {
+    const target = event.target as HTMLTextAreaElement;
+    target.style.height = 'auto';
+    target.style.height = `${target.scrollHeight}px`;
+    trigger('body');
+  };
+
+  const handleTitleCount = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitleCount(event.target.value.length);
+  };
+
+  const handleTitleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    const selectedText = event.target.value;
+
+    if (selectedText === '선택') {
+      setSelectedText('');
+      return;
+    } else {
+      setSelectedText(selectedText);
+    }
+  };
+
+  return (
+    <div className="max-w-[25.875rem] mx-auto max-h-[56rem] h-full pt-[2.75rem] position:relative bg-cooled-blue font-Cafe24SurroundAir">
+      <header className="flex flex-col">
+        <div className="flex justify-between items-center mb-[1.75rem] ml-[2.44rem] mr-[1.56rem]">
+          <HeaderText size="normal" label="글쓰기" />
+          <CloseButton onClick={() => navigate(-1)} />
+        </div>
+      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col h-[48.5rem]  bg-white rounded-t-3xl">
+          <div className="flex items-center justify-between mx-auto w-full max-w-[22.625rem] pt-[2rem]">
+            <Avatar width={2.5} profileImage="" isLoggedIn={false} />
+            <div className="flex w-[12.5rem]">
+              <span className="font-Cafe24Surround">닉네임</span>
+            </div>
+            <button className="w-[6rem] h-[2.2rem] rounded-lg bg-cooled-blue font-Cafe24Surround text-white cursor-pointer">
+              작성하기
+            </button>
+          </div>
+          <div className="max-w-[22.625rem] mx-auto w-full">
+            <div className="flex items-end h-[3.5rem] border-b-2 border-cooled-blue">
+              <select onChange={handleTitleSelect} className="pb-2 mr-2 outline-none text-xs">
+                {DROPDOWN_OPTIONS.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <input
+                {...register('title', {
+                  required: MESSAGE.TITLE_REQUIRED,
+                  minLength: {
+                    value: LENGTH_LIMIT.TITLE_MIN,
+                    message: MESSAGE.TITLE_MINLENGTH,
+                  },
+                  maxLength: {
+                    value: LENGTH_LIMIT.TITLE_MAX,
+                    message: MESSAGE.TITLE_MAXLENGTH,
+                  },
+                  onChange: (e) => {
+                    handleTitleCount(e);
+                    trigger('title');
+                    setSelectedText(e.target.value);
+                  },
+                })}
+                value={selectedText}
+                placeholder={MESSAGE.TITLE_REQUIRED}
+                maxLength={LENGTH_LIMIT.TITLE_MAX}
+                className="block w-full pb-2 outline-none"
+              />
+            </div>
+            <div className="flex justify-end w-full">
+              {errors?.title && (
+                <span className="text-xs text-error-red mr-3">{errors.title.message}</span>
+              )}
+              <span
+                className={`text-xs ${
+                  titleCount > LENGTH_LIMIT.TITLE_MAX ? 'text-error-red' : 'text-lazy-gray'
+                }`}
+              >{`${titleCount}/${LENGTH_LIMIT.TITLE_MAX}`}</span>
+            </div>
+            <div className="block">
+              {image && (
+                <img
+                  className="block w-[3rem] pb-2"
+                  src={URL.createObjectURL(image)}
+                  alt="thumbnail"
+                />
+              )}
+              <textarea
+                {...register('body', {
+                  required: MESSAGE.CONTENT_REQUIRED,
+                  onChange: handleResizeTextareaHeight,
+                  minLength: {
+                    value: LENGTH_LIMIT.CONTENT_MIN,
+                    message: MESSAGE.CONTENT_MINLENGTH,
+                  },
+                  maxLength: {
+                    value: LENGTH_LIMIT.CONTENT_MAX,
+                    message: MESSAGE.CONTENT_MAXLENGTH,
+                  },
+                })}
+                placeholder={MESSAGE.CONTENT_REQUIRED}
+                maxLength={LENGTH_LIMIT.CONTENT_MAX}
+                className="overflow-hidden outline-none resize-none h-[20rem] w-full"
+              />
+              {errors?.body && (
+                <span className="text-xs text-error-red mr-3">{errors.body.message}</span>
+              )}
+            </div>
+          </div>
+          <div className="max-w-[25.875rem] w-full h-[2rem] fixed bottom-4">
+            <label
+              htmlFor="file_input"
+              className="flex items-center justify-center w-[2rem] h-[2rem] rounded-full bg-cooled-blue text-white font-Cafe24SurroundAir absolute right-4 bottom-4 shadow-md cursor-pointer"
+            >
+              <AiOutlinePlus size="1.5rem" />
+            </label>
+            <input
+              id="file_input"
+              type="file"
+              accept="image/*"
+              {...register('image')}
+              onChange={handleChangeImage}
+              className="hidden"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewArticlePage;
