0f383859f43ec470d350925ecb7b6a38cf8fbfbb
Dongho Shin <kakkiid@gmail.com>
2023-09-17 20:44:55 +0900

    chore: daisy ui 테마 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,4 +27,27 @@ export default {
   },
   // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          'input-white': '#FFF',
+          'article-highly-liked': '#24736B',
+          'article-img': '#2D2D2D',
+          'profile-bg': '#EEF1F4',
+          'lazy-gray': '#C0C1C5',
+          'cooled-blue': '#76B7B1',
+          'extra-white': '#EFF0EB',
+          'wall-street': '#697279',
+          'footer-icon': '#455154',
+          'tricorn-black': '#2D2D2D',
+          'error-red': '#CE395F',
+          'light-violet': '#75609E',
+          'tertiory-gray': '#9BA5B7',
+          'primary-black': '#141619',
+        },
+      },
+    ],
+  },
 };
