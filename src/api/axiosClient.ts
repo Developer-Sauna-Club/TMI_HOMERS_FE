4c5ad7c296e6a9e448d858b13e4eb7a3322ff754
정동환 <dongja@jeongdonghwan-ui-MacBookAir.local>
2023-09-17 18:01:44 +0900

    feat: axios 인터셉터 추가

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getItemFromStorage } from '@/utils/localStorage';
 
 const REQUEST_TIME = 3000;
 
@@ -6,3 +7,15 @@ export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_BASEURL,
   timeout: REQUEST_TIME,
 });
+
+const getAuthHeader = () => `bearer ${getItemFromStorage('token')}`;
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    config.headers['Authorization'] = getAuthHeader();
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
