7eed9e13bf2dabfcd5ea1f3cf747bbc0a3e0640f
Dongho Shin <kakkiid@gmail.com>
2023-09-17 17:11:50 +0900

    feat: landing page 구현

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import MainButton from '@/components/MainButton';
 
 const LOGO_SRC = '/img/logo.svg';
-const CHARACTER_SRC = '';
+const CHARACTER_SRC = '/img/character.png';
 const START_BUTTON_CONTENT = '시작하기';
 
 const LandingPage = () => {
@@ -12,15 +12,17 @@ const LandingPage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <img src={LOGO_SRC} alt="logo" />
-        <img src={CHARACTER_SRC} alt="character" />
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-white">
+      <div className="flex flex-col justify-center items-center gap-3">
+        <img src={LOGO_SRC} className="w-[48%] max-w-[300px]" alt="logo" />
+        <img src={CHARACTER_SRC} className="w-[44%] max-w-[500px]" alt="character" />
+        <p className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
+          쓸데없는 것 쓰고 싶을 때
+          <br />
+          쓰고 싶은 곳 호머스
+        </p>
       </div>
-      <div>
-        <p>여러분만의 TMI 뉴스를 마음껏 공유해주세요</p>
-      </div>
-      <div>
+      <div className="mt-[5rem]">
         <MainButton label={START_BUTTON_CONTENT} onClick={handleClickStartButton} />
       </div>
     </div>
