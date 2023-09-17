7ae58d0b0bab61ed519f9e31ab02a04718073568
정동환 <dongja@jeongdonghwan-ui-MacBookAir.local>
2023-09-17 20:45:09 +0900

    fix: 괄호 오타 수정

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,28 +11,29 @@ const START_BUTTON_CONTENT = '시작하기';
 const LandingPage = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
-  
+
   const handleClickStartButton = () => {
     navigate('/home');
   };
-    
+
   if (user === null) {
-    return <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-white">
-      <div className="flex flex-col justify-center items-center gap-3">
-        <img src={LOGO_SRC} className="w-[48%] max-w-[300px]" alt="logo" />
-        <img src={CHARACTER_SRC} className="w-[44%] max-w-[500px]" alt="character" />
-        <p className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
-          {CATCHPHRASE}
-        </p>
+    return (
+      <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-white">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <img src={LOGO_SRC} className="w-[48%] max-w-[300px]" alt="logo" />
+          <img src={CHARACTER_SRC} className="w-[44%] max-w-[500px]" alt="character" />
+          <p className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
+            {CATCHPHRASE}
+          </p>
+        </div>
+        <div className="mt-[5rem]">
+          <MainButton label={START_BUTTON_CONTENT} onClick={handleClickStartButton} />
+        </div>
       </div>
-      <div className="mt-[5rem]">
-        <MainButton label={START_BUTTON_CONTENT} onClick={handleClickStartButton} />
-      </div>
-    </div>
+    );
   }
-    
-    return <Navigate to="/home" replace />;
-  );
+
+  return <Navigate to="/home" replace />;
 };
 
 export default LandingPage;
