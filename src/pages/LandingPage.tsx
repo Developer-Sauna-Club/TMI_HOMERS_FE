cf2fe270e8f775314f9b74b0b3526cc5c958ec7a
Dongho Shin <kakkiid@gmail.com>
2023-09-18 01:04:18 +0900

    fix: 캐치프레이즈 줄바꿈 수정 (#117)
    * fix: 캐치프레이즈 줄바꿈 수정
    
    * fix: user select none

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ import { useAuthContext } from '@/hooks/useAuthContext';
 const LOGO_SRC = '/img/logo.svg';
 const CHARACTER_SRC = '/img/character.png';
 
-const CATCHPHRASE = '쓸데없는 걸 쓰고 싶은데<br/>어디 쓸 데 없나?';
+const CATCHPHRASE = '쓸데없는 걸 쓰고 싶은데\n어디 쓸 데 없나?';
 const START_BUTTON_CONTENT = '시작하기';
 
 const LandingPage = () => {
@@ -19,12 +19,12 @@ const LandingPage = () => {
   if (user === null) {
     return (
       <div className="w-full h-[100vh] flex flex-col items-center justify-center bg-white">
-        <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex flex-col justify-center items-center gap-3 select-none">
           <img src={LOGO_SRC} className="w-[48%] max-w-[300px]" alt="logo" />
           <img src={CHARACTER_SRC} className="w-[44%] max-w-[500px]" alt="character" />
-          <p className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
+          <pre className="font-Cafe24SurroundAir text-[1.25rem] text-wall-street text-center tracking-tighter">
             {CATCHPHRASE}
-          </p>
+          </pre>
         </div>
         <div className="mt-[5rem]">
           <MainButton label={START_BUTTON_CONTENT} onClick={handleClickStartButton} />
