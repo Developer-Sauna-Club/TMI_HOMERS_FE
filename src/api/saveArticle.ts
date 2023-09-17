7734869ea3ad6df6749304fd13db13dc79a590df
colorkite10 <colorkite10@gmail.com>
2023-09-17 14:25:08 +0900

    fix: image type 수정

--- src/api/saveArticle.ts
+++ src/api/saveArticle.ts
@@ -7,14 +7,16 @@ const TOKEN =
 type FormValue = {
   title: string;
   body: string;
-  image: File;
+  image: File | null;
 };
 
 export const saveArticle = async ({ title, body, image }: FormValue) => {
   const formData = new FormData();
   formData.append('title', JSON.stringify({ title: title, body: body }));
   formData.append('channelId', CHANNEL_ID);
-  formData.append('image', image);
+  if (image) {
+    formData.append('image', image);
+  }
 
   await axiosClient.post('/posts/create', formData, {
     headers: {
