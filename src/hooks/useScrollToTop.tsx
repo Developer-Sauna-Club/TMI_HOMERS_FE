4130ed9f805ac069db6f546bfafe4da9e3efddac
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-09-18 19:00:34 +0900

    fix: 위로가기 버튼이 초기 렌더링 되지 않던 오류 수정

--- src/hooks/useScrollToTop.tsx
+++ src/hooks/useScrollToTop.tsx
@@ -1,30 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const useScrollToTop = () => {
   const ref = useRef<HTMLDivElement>(null);
   const [showScrollToTopButton, setShowScrollToTopButton] = useState(false);
 
-  const scrollToTop = () => {
+  // eslint 경고 발생해서 useCallback 사용 (react-hooks/exhaustive-deps)
+  const scrollToTop = useCallback(() => {
     if (ref.current) {
       ref.current.scrollTo({ top: 0, behavior: 'smooth' });
     }
-  };
+  }, [ref]);
 
   useEffect(() => {
-    const currentRef = ref.current;
-    const handleScroll = () => {
-      const scrollPosition = currentRef?.scrollTop || 0;
-      if (scrollPosition > 0) {
-        setShowScrollToTopButton(true);
-      } else {
-        setShowScrollToTopButton(false);
-      }
-    };
-    currentRef?.addEventListener('scroll', handleScroll);
-    return () => {
-      currentRef?.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    if (ref.current) {
+      const currentRef = ref.current;
+      const handleScroll = () => {
+        const scrollPosition = currentRef?.scrollTop || 0;
+        if (scrollPosition > 0) {
+          setShowScrollToTopButton(true);
+        } else {
+          setShowScrollToTopButton(false);
+        }
+      };
+      currentRef?.addEventListener('scroll', handleScroll);
+      return () => {
+        currentRef?.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, [ref, showScrollToTopButton, scrollToTop]);
 
   return { ref, showScrollToTopButton, scrollToTop };
 };
