3c0058bde5545666e689fbeb327b8408953abf61
Dongho Shin <kakkiid@gmail.com>
2023-09-18 18:01:43 +0900

    fix: 폼 인풋 컴포넌트 폰트 노말로 수정

--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -4,13 +4,13 @@ type HeaderTextProps = {
 };
 
 const HeaderText = ({ size = 'normal', label }: HeaderTextProps) => {
-  const TEXT_BASE_CLASS = 'font-Cafe24Surround text-tricorn-black inline-block';
+  const TEXT_BASE_CLASS = 'font-Cafe24Surround text-tricorn-black inline-block dark:text-white';
   const TEXT_SIZE_CLASS = {
     small: 'text-[1.75rem]',
     normal: 'text-[2rem]',
     large: 'text-[2.5rem]',
   };
-  return <h1 className={`dark:text-white ${TEXT_BASE_CLASS} ${TEXT_SIZE_CLASS[size]}`}>{label}</h1>;
+  return <h1 className={`${TEXT_BASE_CLASS} ${TEXT_SIZE_CLASS[size]}`}>{label}</h1>;
 };
 
 export default HeaderText;
