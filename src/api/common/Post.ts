37a9231ef1b0d35038887b5f46d3414812ff034b
Dongja <jdh981118@naver.com>
2023-09-18 16:26:43 +0900

    Feat : api 함수 추가 (#119)
    * feat: axios 인터셉터 추가
    
    * refactor: 회원가입 페이지에서 회원가입 폼 분리
    
    * feat: auth api 함수 추가
    
    * feat: user api 추가
    
    * feat: post, UserSetting 공통 api  추가
    
    * feat: deletepost api 추가
    
    * feat: comment, Like api 추가
    
    * feat: 팔로우, 알림, 검색 api 추가
    
    * refactor: checkauth 하는 함수 수정
    
    ---------
    
    Co-authored-by: 정동환 <dongja@jeongdonghwan-ui-MacBookAir.local>

---
+++ src/api/common/Post.ts
@@ -0,0 +1,105 @@
+import type { Post } from '@/type/Post';
+import { axiosClient } from '../axiosClient';
+
+type InfinitiyScrollParams = {
+  offset: number;
+  limit: number;
+};
+
+export const fetchAllPosts = async ({ offset, limit }: Partial<InfinitiyScrollParams>) => {
+  const FETCH_ALL_POSTS_URL = '/posts/channel/64fac2e729260903240d2dab';
+
+  const { data } = await axiosClient.get<Post[]>(FETCH_ALL_POSTS_URL, {
+    params: { offset, limit },
+  });
+  return data;
+};
+
+export const fetchUserPosts = async ({
+  offset,
+  limit,
+  authorId,
+}: Partial<InfinitiyScrollParams> & { authorId: string }) => {
+  const FETCH_USER_POSTS_URL = `/posts/author/${authorId}`;
+
+  const { data } = await axiosClient.get<Post[]>(FETCH_USER_POSTS_URL, {
+    params: { offset, limit },
+  });
+  return data;
+};
+
+type CreatePostParams = {
+  title: string;
+  image: File | null;
+  channelId: string;
+};
+
+export const createPost = async ({ title, image, channelId }: CreatePostParams) => {
+  const CREATE_POST_URL = '/posts/create';
+  const { data } = await axiosClient.post<Post>(
+    CREATE_POST_URL,
+    {
+      title,
+      image,
+      channelId,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+  return data;
+};
+
+export const fetchPost = async (postId: string) => {
+  const FETCH_POST_URL = `/posts/${postId}`;
+
+  const { data } = await axiosClient.get<Post>(FETCH_POST_URL);
+  return data;
+};
+
+type UpdatePostParams = {
+  postId: string;
+  title: string;
+  image: File | null;
+  imageToDeletePublicId?: string;
+  channelId: string;
+};
+
+export const updatePost = async ({
+  postId,
+  title,
+  image,
+  imageToDeletePublicId,
+  channelId,
+}: UpdatePostParams) => {
+  const UPDATE_POST_URL = '/posts/update';
+
+  const { data } = await axiosClient.put(
+    UPDATE_POST_URL,
+    {
+      postId,
+      title,
+      image,
+      imageToDeletePublicId,
+      channelId,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+  return data;
+};
+
+export const deletePost = async (id: string) => {
+  const DELETE_POST_URL = '/posts/delete';
+
+  return await axiosClient.delete(DELETE_POST_URL, {
+    data: {
+      id,
+    },
+  });
+};
