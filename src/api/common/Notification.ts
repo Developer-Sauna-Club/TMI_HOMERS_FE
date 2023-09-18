c6f327d26732e8d3dedb6f39430b093b1ab2f7f2
정동환 <dongja@jeongdonghwan-ui-MacBookAir.local>
2023-09-18 12:06:44 +0900

    feat: 팔로우, 알림, 검색 api 추가

---
+++ src/api/common/Notification.ts
@@ -0,0 +1,38 @@
+import { axiosClient } from '../axiosClient';
+import { Notification } from './../../type/Notification';
+
+export const fetchNotifications = async () => {
+  const FETCH_NOTIFICATIONS_URL = '/notifications';
+  const { data } = await axiosClient.get<Notification[]>(FETCH_NOTIFICATIONS_URL);
+  return data;
+};
+
+export const readNotifications = async () => {
+  const READ_NOTIFICATIONS = '/notifications/seen';
+  return await axiosClient.put<Notification>(READ_NOTIFICATIONS);
+};
+
+type NotificationType = 'COMMENT' | 'FOLLOW' | 'LIKE' | 'MESSAGE';
+
+type NotificationParam = {
+  notificationType: NotificationType;
+  notificationTypeId: string;
+  userId: string;
+  postId: string | null;
+};
+
+export const createNotification = async ({
+  notificationTypeId,
+  notificationType,
+  userId,
+  postId,
+}: NotificationParam) => {
+  const CREATE_NOTIFICATION = '/notifications/create';
+  const { data } = await axiosClient.post<Notification>(CREATE_NOTIFICATION, {
+    notificationType,
+    notificationTypeId,
+    userId,
+    postId,
+  });
+  return data;
+};
