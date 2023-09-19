f1d5a0ce29575676c59890ad543260ac0c13eae1
Dongho Shin <kakkiid@gmail.com>
2023-09-19 23:54:37 +0900

    remove: 기존 모달 예시 제거

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,16 +1,5 @@
-import Modal from '@/components/Modal';
-
 const LandingPage = () => {
-  return (
-    <>
-      <h2>랜딩 페이지입니다</h2>
-      <Modal
-        title="정말 삭제하시겠습니까?"
-        body="삭제된 내용은 복구할 수 없습니다."
-        confirmLabel="삭제"
-      />
-    </>
-  );
+  return <h2>랜딩 페이지입니다</h2>;
 };
 
 export default LandingPage;
