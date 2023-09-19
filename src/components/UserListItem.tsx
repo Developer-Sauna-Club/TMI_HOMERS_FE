32d36ea24678316af3638b9905bd4eeb500589ab
김다은 <GBAJS754@gmail.com>
2023-09-20 00:28:47 +0900

    feat: 팔로우, 언팔로우 api 연동

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,12 +1,25 @@
+import { followUser, unFollowUser } from '@/api/common/Follow';
 import { UserListItemParams } from '@/type/search';
 import { ARTICLE_TITLE_MAX_LENGTH } from '@constants/Article';
+import { useAuthContext } from '@hooks/useAuthContext';
 import Avatar from './Avatar';
 import SubButton from './SubButton';
-
 const SEARCH_RESULT_CLASS =
   'cursor-pointer max-w-[22.375rem] mb-[0.8rem] mt-[0.5rem] mx-auto flex items-center justify-between font-Cafe24SurroundAir pl-4 pr-3 pb-[0.625rem] pt-[0.25rem]';
 
 const UserListItem = ({ fullName, id }: UserListItemParams) => {
+  const { user } = useAuthContext();
+
+  const handleUnFollowing = (id: string) => {
+    const userId = user?.following.find(({ user }) => user === id);
+    if (userId) {
+      unFollowUser(userId._id);
+    }
+  };
+  const handleFollowing = (id: string) => {
+    followUser(id);
+  };
+
   return (
     <div key={id} className={`${SEARCH_RESULT_CLASS}`}>
       <div className="flex items-center gap-4 line-clamp-1">
@@ -17,7 +30,25 @@ const UserListItem = ({ fullName, id }: UserListItemParams) => {
             : fullName}
         </div>
       </div>
-      <SubButton label="팔로우" color="violet" type="outline" size="small" />
+      {user?.following.every(({ user }) => user !== id) ? (
+        <SubButton
+          key={id}
+          label="팔로우"
+          color="violet"
+          type="outline"
+          size="small"
+          onClick={() => handleFollowing(id)}
+        />
+      ) : (
+        <SubButton
+          key={id}
+          label="팔로우"
+          color="violet"
+          type="fill"
+          size="small"
+          onClick={() => handleUnFollowing(id)}
+        />
+      )}
     </div>
   );
 };
