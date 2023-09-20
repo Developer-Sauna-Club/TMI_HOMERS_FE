bfd8d17efd39363965aca3ad8c35e7d439004490
Dongho Shin <kakkiid@gmail.com>
2023-09-21 01:39:56 +0900

    feat: 토스트 메시지 일괄 적용2

--- src/hooks/useArticleDetail.tsx
+++ src/hooks/useArticleDetail.tsx
@@ -3,11 +3,14 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Post } from '@type/Post';
 import { CommentParams, createComment } from '@/api/common/Comment';
 import { fetchPost } from '@/api/common/Post';
+import { TOAST_MESSAGES } from '@/constants/Messages';
+import { useToastContext } from './useToastContext';
 
 export const useArticleDetail = () => {
   const queryClient = useQueryClient();
   const { pathname: url } = useLocation();
   const postId = url.split('/').pop() || '';
+  const { showToast } = useToastContext();
 
   const { data, isLoading } = useQuery<Post>(
     ['article', postId],
@@ -23,6 +26,9 @@ export const useArticleDetail = () => {
   const commentMutation = useMutation(createComment, {
     onSuccess: () => {
       queryClient.invalidateQueries(['article', postId]);
+    },
+    onError: () => {
+      showToast(TOAST_MESSAGES.DELETE_COMMENT_FAILED, 'error');
     },
   });
 
