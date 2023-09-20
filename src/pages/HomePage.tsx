d97aae1964a10f47a46d0161d8040db53bd81752
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-09-20 18:46:41 +0900

    feat: 홈페이지 게시물 불러오기 구현 (#157)
    * design: 하단 네비게이션 고정
    
    * feat: 게시물 불러오기
    
    * refactor: 상수분리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,65 +1,63 @@
+import { useNavigate } from 'react-router-dom';
 import { HiFire } from 'react-icons/hi';
 import { MdOutlineSearch } from 'react-icons/md';
-import Article from '@components/Article';
+import Loader from '@/components/Loader';
+import { API } from '@/constants/Article';
+import { MESSAGE, POST_COUNT } from '@/constants/Home';
+import { TAB_CONSTANTS } from '@/constants/Tab';
+import { useArticles } from '@/hooks/useArticles';
+import { useFilteredArticles } from '@/hooks/useFilteredArticles';
 import BottomNavigation from '@components/BottomNavigation';
 import HeaderText from '@components/HeaderText';
+import Articles from './ArticlesPage/Articles';
 
 const CHARACTER_SRC = '/img/character.png';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const { data: articles, isFetching } = useArticles({
+    id: API.CHANNEL_ID,
+    type: 'channel',
+  });
+
+  const newestArticles = useFilteredArticles(TAB_CONSTANTS.NEWEST, articles).slice(
+    0,
+    POST_COUNT.NEWEST,
+  );
+  const hottestArticles = useFilteredArticles(TAB_CONSTANTS.HOTTEST, articles).slice(
+    0,
+    POST_COUNT.HOTTEST,
+  );
+
   return (
     <div className="relative flex flex-col justify-center items-center overflow-hidden">
       <div className="w-full max-w-md flex flex-col gap-36 overflow-y-scroll">
         <section className="bg-cooled-blue h-[375px] mb-10">
           <header className="flex h-[180px] justify-between px-10 items-center">
-            <HeaderText label="홈" />
-            <MdOutlineSearch size="24" className="text-tricorn-black" />
+            <HeaderText label={MESSAGE.HOME} />
+            <MdOutlineSearch size="24" className="text-tricorn-black cursor-pointer" />
           </header>
           <section className="relative w-full h-[304px] gap-2 flex justify-center">
             <div className="w-10/12 max-w-[374px] flex flex-col gap-2">
               <div className="flex items-center">
                 <HiFire size="24" className="text-article-highly-liked" />
                 <h2 className="flex-none text-tricorn-black font-Cafe24Surround text-lg font-bold">
-                  뜨거운 뉴스
+                  <span onClick={() => navigate('/news')} className="cursor-pointer">
+                    {MESSAGE.HOTTEST_NEWS}
+                  </span>
                 </h2>
               </div>
               <div className="bg-white text-black w-full rounded-xl shadow-article-container max-w-sm self-center h-[304px] z-20">
-                <Article
-                  title="되겠냐?"
-                  id="1"
-                  nickname="@khakhiD"
-                  postedDate="2023-08-29T09:28:39.390Z"
-                  hasImage={true}
-                  likes={0}
-                  comments={1}
-                />
-                <Article
-                  title="되겠냐?"
-                  id="1"
-                  nickname="@khakhiD"
-                  postedDate="2023-08-29T09:28:39.390Z"
-                  hasImage={true}
-                  likes={0}
-                  comments={1}
-                />
-                <Article
-                  title="되겠냐?"
-                  id="1"
-                  nickname="@khakhiD"
-                  postedDate="2023-08-29T09:28:39.390Z"
-                  hasImage={true}
-                  likes={0}
-                  comments={1}
-                />
-                <Article
-                  title="되겠냐?"
-                  id="1"
-                  nickname="@khakhiD"
-                  postedDate="2023-08-29T09:28:39.390Z"
-                  hasImage={true}
-                  likes={0}
-                  comments={1}
-                />
+                <div>
+                  {isFetching ? (
+                    <div className="flex justify-center">
+                      <Loader />
+                    </div>
+                  ) : (
+                    <Articles articles={hottestArticles} />
+                  )}
+                </div>
               </div>
               <img
                 src={CHARACTER_SRC}
@@ -70,89 +68,26 @@ const HomePage = () => {
           </section>
         </section>
         <section className=" bg-white flex flex-col justify-center gap-6 flex-grow">
-          <div className="bg-emerald-300 w-[280px] h-20 self-center" />
+          {/* <div className="bg-emerald-300 w-[280px] h-20 self-center" /> */}
           <div className="flex flex-col gap-3">
             <h2 className="text-tricorn-black font-Cafe24Surround text-lg font-bold px-7">
-              최신 이야기
+              <span onClick={() => navigate('/news')} className="cursor-pointer">
+                {MESSAGE.NEWEST}
+              </span>
             </h2>
-            <ul>
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-              <Article
-                title="되겠냐?"
-                id="1"
-                nickname="@khakhiD"
-                postedDate="2023-08-29T09:28:39.390Z"
-                hasImage={true}
-                likes={0}
-                comments={1}
-              />
-            </ul>
+            <div>
+              {isFetching ? (
+                <div className="flex justify-center">
+                  <Loader />
+                </div>
+              ) : (
+                <Articles articles={newestArticles} />
+              )}
+            </div>
           </div>
         </section>
       </div>
-      <footer className="sticky bottom-0 flex-none justify-center items-center">
+      <footer className="fixed bottom-0 flex-none justify-center items-center">
         <BottomNavigation currentPage="/home" />
       </footer>
     </div>
