705dee08ec83aa1a1d3f95fe76653ce8cd8955eb
DongjaJ <jdh981118@naver.com>
2023-09-21 15:02:37 +0900

    feat: 프로필 수정 페이지에서 x 버튼 눌렀을때 모달 나오는 기능 추가

--- src/pages/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage.tsx
@@ -1,27 +1,37 @@
 import { useNavigate } from 'react-router-dom';
 import CloseButton from '@/components/CloseButton';
 import HeaderText from '@/components/HeaderText';
+import Confirm from '@/components/Modals/Confirm';
 import useAuthQuery from '@/hooks/useAuthQuery';
+import useModal from '@/hooks/useModal';
 import type { User } from '@/type/User';
 import EditForm from './ProfileEditPage/EditForm';
 
 const ProfileEditPage = () => {
+  const navigate = useNavigate();
+  const { showModal, modalOpen, modalClose } = useModal();
   const {
     userQuery: { data: user },
   } = useAuthQuery();
 
-  const navigate = useNavigate();
-
-  const handleClickCloseButton = () => {
+  const handleClickConfirm = () => {
     navigate(-1);
   };
 
   return (
     <section className="grid grid-rows-[1fr_8fr] h-screen overflow-y-auto">
+      {showModal && (
+        <Confirm
+          theme="negative"
+          title="수정사항이 사라질 수 있어요"
+          onClose={modalClose}
+          onConfirm={handleClickConfirm}
+        />
+      )}
       <header className="flex justify-center w-full">
         <div className="flex items-center justify-between w-full max-w-sm p-4 pt-8">
           <HeaderText label="프로필 수정" />
-          <CloseButton onClick={handleClickCloseButton} />
+          <CloseButton onClick={modalOpen} />
         </div>
       </header>
       <EditForm user={user! as User} />
