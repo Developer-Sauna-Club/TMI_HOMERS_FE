091be3cc3e1a772af0d31aa4cc58f6632f73c37b
DongjaJ <jdh981118@naver.com>
2023-09-21 14:46:54 +0900

    feat: 프로필 수정 페이지에서 x버튼 눌렀을 때 이전 페이지로 이동하도록 수정

--- src/pages/ProfileEditPage.tsx
+++ src/pages/ProfileEditPage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import CloseButton from '@/components/CloseButton';
 import HeaderText from '@/components/HeaderText';
 import useAuthQuery from '@/hooks/useAuthQuery';
@@ -9,12 +10,18 @@ const ProfileEditPage = () => {
     userQuery: { data: user },
   } = useAuthQuery();
 
+  const navigate = useNavigate();
+
+  const handleClickCloseButton = () => {
+    navigate(-1);
+  };
+
   return (
     <section className="grid grid-rows-[1fr_8fr] h-screen overflow-y-auto">
       <header className="flex justify-center w-full">
         <div className="flex items-center justify-between w-full max-w-sm p-4 pt-8">
           <HeaderText label="프로필 수정" />
-          <CloseButton />
+          <CloseButton onClick={handleClickCloseButton} />
         </div>
       </header>
       <EditForm user={user! as User} />
