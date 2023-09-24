cab410ef5347efb022b8b4ad7bd778616719ed3f
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-09-24 13:46:25 +0900

    refactor: 글 작성 페이지, 글 상세 페이지  (#207)
    * design: 사진첨부 버튼 사이즈 크게 변경
    
    * feat: 사진 첨부한 것 삭제하기
    
    * feat: 글 수정 페이지에서 사진 제거 기능 추가
    
    * refactor: 글 수정 이미지 삭제 기능 + ImagePreview 컴포넌트로 코드 분리
    
    * fix: imagePreview 컴포넌트 수정
    
    * feat: 작성하기 버튼 누를 시 완료될 때까지 버튼 비활성화
    
    * feat: 글 상세 페이지에서 사진 클릭하면 확대됨
    
    * feat: 글 상세 페이지에서 사용자 사진 혹은 닉네임 누를 시 프로필 이동
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { getTimeStamp } from '@utils/getTimeStamp';
 import Avatar from './Avatar';
 import CloseButton from './CloseButton';
@@ -9,6 +10,7 @@ type CommentProps = {
   postedDate: string;
   active: boolean;
   profileImage: string;
+  authorId: string;
   onDelete: (commentId: string) => void;
 };
 
@@ -19,19 +21,31 @@ const Comment = ({
   postedDate,
   active,
   profileImage,
+  authorId,
   onDelete,
 }: CommentProps) => {
   const timestamp = getTimeStamp(postedDate);
+  const navigate = useNavigate();
 
   return (
     <div className="flex justify-center items-center w-[20rem] min-h-[6rem] mt-3 text-tricorn-black dark:text-extra-white">
       <div className="flex flex-col w-[20rem]  font-Cafe24SurroundAir">
         <div className="flex w-[20rem] h-[1.5rem]">
           <div className="icon cursor-pointer">
-            <Avatar width={1.5} profileImage={profileImage} isLoggedIn={false} />
+            <Avatar
+              width={1.5}
+              profileImage={profileImage}
+              isLoggedIn={false}
+              onClick={() => navigate(`/profile/${authorId}`)}
+            />
           </div>
           <div className="ml-2 w-[18rem] text-wall-street dark:text-extra-white text-base">
-            <span className="text-sm cursor-pointer">{nickname}</span>
+            <span
+              className="text-sm cursor-pointer"
+              onClick={() => navigate(`/profile/${authorId}`)}
+            >
+              {nickname}
+            </span>
           </div>
           <div className="flex w-[1rem]">
             {active && <CloseButton mode="small" onClick={() => onDelete(commentId)} />}
