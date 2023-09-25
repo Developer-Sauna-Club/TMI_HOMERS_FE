a75fa639cdb99fd7b20064b00650fba45f3662aa
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-09-25 14:13:50 +0900

    fix: 좋아요 취소 누를 시 알림 가던 문제 해결 (#220)
    * fix: 좋아요 취소시 알림 가던 것 수정
    
    * fix: 수정 페이지 타입 임시 수정

--- src/pages/ArticleDetailPage.tsx
+++ src/pages/ArticleDetailPage.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsTrash } from 'react-icons/bs';
 import { FiEdit } from 'react-icons/fi';
-import { useLikeCreateMutation, useLikeDeleteMutation } from '@/hooks/useLikeMutation';
 import { useNotification } from '@/hooks/useNotification';
 import { deletePost } from '@api/common/Post';
 import ArticleDetail from '@components/ArticleDetail';
@@ -13,6 +12,7 @@ import SubButton from '@components/SubButton';
 import { BUTTON, MESSAGE } from '@constants/ArticleDetail';
 import { useArticleDetail } from '@hooks/useArticleDetail';
 import useAuthQuery from '@hooks/useAuthQuery';
+import { useLikeCreateMutation, useLikeDeleteMutation } from '@hooks/useLikeMutation';
 import CommentInput from './ArticleDetailPage/CommentInput';
 import Comments from './ArticleDetailPage/Comments';
 
@@ -25,7 +25,7 @@ const ArticleDetailPage = () => {
   const { data: article, isLoading, addComment } = useArticleDetail();
   const { mutate: likeCreateMutate, isLoading: isLikeCreateLoading } = useLikeCreateMutation();
   const { mutate: likeDeleteMutate, isLoading: isLikeDeleteLoading } = useLikeDeleteMutation();
-  const { mutate: likeNotificationMutate } = useNotification();
+  const { mutate: likeNotificationMutate, isLoading: isLikeNotificationLoading } = useNotification();
   const [isBigImage, setIsBigImage] = useState(false);
 
   if (isLoading) {
@@ -42,22 +42,23 @@ const ArticleDetailPage = () => {
 
   const toggleLikeMutate = () => {
     if (myLike) {
-      likeDeleteMutate(myLike._id, {
-        onSuccess: (newLike) =>
+      likeDeleteMutate(myLike._id);
+    } else {
+      likeCreateMutate(_id, {
+        onSuccess: (newLike) => {
           likeNotificationMutate({
             notificationType: 'LIKE',
             notificationTypeId: newLike._id,
             userId: postUserId,
             postId: newLike.post,
-          }),
+          });
+        },
       });
-    } else {
-      likeCreateMutate(_id);
     }
   };
 
   const handleLikeButtonClick = () => {
-    if (isLikeCreateLoading || isLikeDeleteLoading) {
+    if (isLikeCreateLoading || isLikeDeleteLoading || isLikeNotificationLoading) {
       return;
     }
     if (!isLoginUser) {
