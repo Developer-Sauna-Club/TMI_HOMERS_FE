b42711bf8a9ef47c6252c0c6bb50d3c04c26a495
DongjaJ <jdh981118@naver.com>
2023-09-27 15:42:22 +0900

    fix: 다크 모드 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { getItemFromStorage, setItemToStorage } from '@/utils/localStorage';
+import { useThemeContext } from '@hooks/useThemeContext';
 import { ToastContextProvider } from './context/ToastContext';
 import MockUpPage from './pages/MockUpPage';
 
 const App = () => {
+  const { theme, updateDarkMode } = useThemeContext();
+
+  useEffect(
+    () => {
+      if (
+        getItemFromStorage('theme') === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ) {
+        updateDarkMode('dark');
+      } else {
+        updateDarkMode('light');
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    setItemToStorage('theme', theme);
+  }, [theme]);
+
   return (
     <div className="lg:w-full lg:h-screen lg:flex lg:justify-center">
       <MockUpPage>
