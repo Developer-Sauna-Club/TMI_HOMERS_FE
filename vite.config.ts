317406ccaf000e9ceeeaef83098b7fc887f80635
Dongja <jdh981118@naver.com>
2023-11-12 03:06:12 +0900

    refactor : 번들 사이즈 줄이기 (#318)
    * refactor: 코드 스플리팅 적용
    
    * refactor: page suspense 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { dependencies } from './package.json';
+function renderChunks(deps: Record<string, string>) {
+  const chunks: { [key: string]: string[] } = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) {
+      return;
+    }
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +29,16 @@ export default defineConfig({
       { find: '@type', replacement: path.resolve(__dirname, 'src/type') },
       { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
     ],
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
