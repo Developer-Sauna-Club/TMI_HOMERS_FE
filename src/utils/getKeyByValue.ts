34850b4bb8b4023013fba2ff5de4ac511e395933
DongjaJ <jdh981118@naver.com>
2024-01-10 19:05:32 +0900

    test: 예제 테스트 추가

--- src/utils/getKeyByValue.ts
+++ src/utils/getKeyByValue.ts
@@ -4,7 +4,7 @@ export function getKeyByValue<T extends { [index: string]: string }>(
 ): keyof T | null {
   for (const key in enumObj) {
     if (enumObj[key] === value) {
-      return key as keyof T;
+      return key;
     }
   }
   return null;
