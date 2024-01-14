a69edca63a0d05d7b95d380095c8f5ced7e8719e
DongjaJ <jdh981118@naver.com>
2024-01-14 12:15:37 +0900

    remove: 필요 없는 api 함수들 삭제

--- src/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from '@/type/User';
-import { axiosClient } from './axiosClient';
-
-const CHECK_AUTH_URL = '/auth-user';
-
-export const checkAuthentication = async () => {
-  const response = await axiosClient.get<User>(CHECK_AUTH_URL);
-  return response.data;
-};
--- src/api/getUserInfo.ts
+++
@@ -1,8 +0,0 @@
-import { axiosClient } from './axiosClient';
-
-const getUserInfo = async (userId: string) => {
-  const userInfo = await axiosClient.get(`/users/${userId}`);
-  return userInfo.data;
-};
-
-export default getUserInfo;
