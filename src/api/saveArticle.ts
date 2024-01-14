0a6af88535cd72d1a710fcf9629a65623ff1764c
DongjaJ <jdh981118@naver.com>
2024-01-14 13:19:01 +0900

    fix: channel_ID constant 이용하도록 수정

--- src/api/saveArticle.ts
+++ src/api/saveArticle.ts
@@ -1,6 +1,5 @@
+import { API } from '@/constants/Article';
 import { axiosClient } from './axiosClient';
-
-export const CHANNEL_ID = '6512cdce2692b6795f161818';
 
 type FormValue = {
   title: string;
@@ -11,7 +10,7 @@ type FormValue = {
 export const saveArticle = async ({ title, body, image }: FormValue) => {
   const formData = new FormData();
   formData.append('title', JSON.stringify({ title: title, body: body }));
-  formData.append('channelId', CHANNEL_ID);
+  formData.append('channelId', API.CHANNEL_ID);
   if (image) {
     formData.append('image', image);
   }
